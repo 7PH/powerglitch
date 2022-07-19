c3e7a2f7d94e5a89b508fe5b936ad9faf28ef114
7PH <b.raymond@protonmail.com>
2022-07-19 23:48:15 +0200

    Fix vite build base path for github pages & Add empty README

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
+    base: '/powerglitch/',
+
     root: 'docs-src/',
     build: {
         outDir: '../docs',
