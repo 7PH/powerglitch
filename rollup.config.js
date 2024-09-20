2e04a465b3fc10d306875908f850ff3e51d84793
7PH <b.raymond@protonmail.com>
2024-09-20 19:57:35 +0200

    Provide both ESM & CJS bundles

--- rollup.config.js
+++ rollup.config.js
@@ -2,21 +2,37 @@ import esbuild, { minify } from 'rollup-plugin-esbuild';
 
 const name = 'powerglitch';
 
-const bundle = config => ({
+const bundle = (config) => ({
     ...config,
+    plugins: [esbuild(), minify()],
     input: 'src/index.ts',
-    external: id => !/^[./]/.test(id),
+    external: (id) => !/^[./]/.test(id),
 });
 
 export default [
     bundle({
-        plugins: [esbuild(), minify()],
         output: [
             {
                 file: `dist/${name}.min.js`,
                 name: 'window',
                 extend: true,
                 format: 'iife',
+            },
+        ],
+    }),
+    bundle({
+        output: [
+            {
+                file: 'lib/index.esm.js',
+                format: 'esm',
+            },
+        ],
+    }),
+    bundle({
+        output: [
+            {
+                file: 'lib/index.cjs',
+                format: 'cjs',
             },
         ],
     }),
